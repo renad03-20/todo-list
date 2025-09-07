75b1c043acf5bc2c85b8615bb7b4f14cac5967ed
Rinad adel <studentrenad@gmail.com>
2025-09-07 23:54:05 +0300

    configration for static files

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "todo_app/static")
+]
+
