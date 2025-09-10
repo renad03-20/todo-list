fd09743fb4fc0f521c2b38732af0a6ca45f9e19b
Rinad adel <studentrenad@gmail.com>
2025-09-10 23:41:07 +0300

    add the urls for complting and deleting tasks

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('toggle_complete/', views.toggle_complete, name='toggle_complete_ajax'),
+    path('delete_task/', views.delete_task_ajax, name='delete_task_ajax'),
     path('register/', views.register, name='register'),
     path('login/', views.loginpage, name='login')
 ]
