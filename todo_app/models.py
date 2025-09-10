19ecbffcb69ab484f11a61a9b4dee9e73701d892
Rinad adel <studentrenad@gmail.com>
2025-09-10 23:43:05 +0300

    add complete column[D to task table

--- todo_app/models.py
+++ todo_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     task = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.task
