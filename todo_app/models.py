55aa7050ab847469f9ea243d3d977c762490b7d3
Rinad adel <studentrenad@gmail.com>
2025-09-13 23:48:06 +0300

    add user column as a foreign key

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
