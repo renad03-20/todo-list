24fe57ec013121b79c36852323f8b95dbf5dbbd9
Rinad adel <studentrenad@gmail.com>
2025-09-13 23:46:23 +0300

    add logout page url

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('toggle_complete/', views.toggle_complete, name='toggle_complete_ajax'),
     path('delete_task/', views.delete_task_ajax, name='delete_task_ajax'),
     path('register/', views.register, name='register'),
-    path('login/', views.loginpage, name='login')
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutpage, name='logout'),
 ]
